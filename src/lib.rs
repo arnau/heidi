00c2cc21d0a94f0ff5ecd31a385467899d55b625
Arnau Siches <asiches@gmail.com>
2020-04-13 09:23:48 +0100

    docs: Document the Number type with examples
    Signed-off-by: Arnau Siches <asiches@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,56 @@
 // This file may not be copied, modified, or distributed except
 // according to those terms.
 
-/// `nhs_number` implements the NSH number validation “Modulus 11”.
-/// See: https://www.datadictionary.nhs.uk/data_dictionary/attributes/n/nhs/nhs_number_de.asp
+//! `nhs_number` implements the NSH number validation “Modulus 11”.
+//! See: <https://www.datadictionary.nhs.uk/data_dictionary/attributes/n/nhs/nhs_number_de.asp>
+//!
+//! Example numbers were generated with <http://danielbayley.uk/nhs-number/>
+
 use std::convert::TryFrom;
 use std::error::Error;
+use std::str::FromStr;
 
 /// A digit can be from 0 to 9.
-type Digit = u16;
+pub type Digit = u16;
 
+/// Represents an NHS Number as a list of 9 digits (`Number.digits()`) plus 1
+/// check digit (`Number.checkdigit()`).
+///
+/// # Examples
+///
+/// ```
+/// use nhs_number::Number;
+/// use std::str::FromStr;
+///
+/// let n = "6541003238";
+/// let number = Number::from_str(n);
+///
+/// assert_eq!(*number.unwrap().checkdigit(), 8);
+/// ```
+///
+/// Or from a `String`:
+///
+/// ```
+/// use std::convert::TryFrom;
+/// use nhs_number::Number;
+///
+/// let n = String::from("6541003238");
+/// let number = Number::try_from(n);
+///
+/// assert_eq!(*number.unwrap().checkdigit(), 8);
+/// ```
+///
+/// Finally, with a `u16` slice:
+///
+/// ```
+/// use std::convert::TryFrom;
+/// use nhs_number::Number;
+///
+/// let n: [u16; 10] = [6, 5, 4, 1, 0, 0, 3, 2, 3, 8];
+/// let number = Number::try_from(&n);
+///
+/// assert_eq!(*number.unwrap().checkdigit(), 8);
+/// ```
 #[derive(PartialEq, Clone, Debug)]
 pub struct Number {
     digits: [Digit; 9],
@@ -19,7 +61,21 @@ pub struct Number {
 }
 
 impl Number {
-    fn new(digits: [Digit; 9]) -> Result<Self, ValidationError> {
+    /// Creates a new Number from the main 9 digits.
+    ///
+    /// Prefer `FromStr` or `TryFrom<[Digit; 10]>` if you have a full NHS number.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use nhs_number::Number;
+    ///
+    /// let n: [u16; 9] = [3, 7, 8, 3, 9, 5, 5, 6, 0];
+    /// let number = Number::new(n);
+    ///
+    /// assert_eq!(*number.unwrap().checkdigit(), 2);
+    /// ```
+    pub fn new(digits: [Digit; 9]) -> Result<Self, ValidationError> {
         Ok(Number {
             checkdigit: check_digit(&digits)?,
             digits,
@@ -38,6 +94,23 @@ impl Number {
 impl TryFrom<&[Digit; 10]> for Number {
     type Error = ValidationError;
 
+    /// Converts an array slice of 10 decimal `u16` into a [`Number`].
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use nhs_number::Number;
+    /// use std::convert::TryFrom;
+    ///
+    /// let n: [u16; 10] = [6, 5, 4, 1, 0, 0, 3, 2, 3, 8];
+    /// let number = Number::try_from(&n);
+    ///
+    /// assert_eq!(*number.unwrap().checkdigit(), 8);
+    /// ```
+    ///
+    /// # Errors
+    ///
+    /// Fails with [ValidationError] when the check digit cannot be verified.
     fn try_from(value: &[Digit; 10]) -> Result<Self, Self::Error> {
         let control = value.last().expect("The given slice is empty!");
         let mut digits: [Digit; 9] = [0; 9];
@@ -58,17 +131,54 @@ impl TryFrom<&[Digit; 10]> for Number {
     }
 }
 
-use std::str::FromStr;
+impl TryFrom<String> for Number {
+    type Error = ValidationError;
+
+    /// Converts a string of 10 digits into a [`Number`].
+    ///
+    /// ```
+    /// use nhs_number::Number;
+    /// use std::convert::TryFrom;
+    ///
+    /// let n = String::from("6541003238");
+    /// let number = Number::try_from(n);
+    ///
+    /// assert_eq!(*number.unwrap().checkdigit(), 8);
+    /// ```
+    ///
+    /// # Errors
+    ///
+    /// Fails with [ValidationError] when the check digit cannot be verified.
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        Number::from_str(&value)
+    }
+}
+
 impl FromStr for Number {
     type Err = ValidationError;
 
+    /// Converts a string slice of 10 digits into a [`Number`].
+    ///
+    /// ```
+    /// use nhs_number::Number;
+    /// use std::str::FromStr;
+    ///
+    /// let n = "6541003238";
+    /// let number = Number::from_str(n);
+    ///
+    /// assert_eq!(*number.unwrap().checkdigit(), 8);
+    /// ```
+    ///
+    /// # Errors
+    ///
+    /// Fails with [ValidationError] when the check digit cannot be verified.
     fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut digits: [Digit; 10] = [0; 10];
+        let vec: Vec<Digit> = s.chars().map(|d| d.to_digit(10).unwrap() as u16).collect();
+
         if s.len() != 10 {
             return Err(ValidationError::new("Numbers must be of 10 digits."));
         }
-
-        let mut digits: [Digit; 10] = [0; 10];
-        let vec: Vec<Digit> = s.chars().map(|d| d.to_digit(10).unwrap() as u16).collect();
 
         digits.copy_from_slice(&vec);
 
